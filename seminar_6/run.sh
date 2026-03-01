e11f24bf953c32759cab0c96166376a50c903382
antwoor <geroev_sasha@mail.ru>
2026-03-01 16:43:59 +0300

    run script for seminar 6 added

---
+++ seminar_6/run.sh
@@ -0,0 +1,9 @@
+xhost +local:docker
+docker run -it \
+    --rm \
+    --network host \
+    --env DISPLAY=$DISPLAY \
+    --env QT_X11_NO_MITSHM=1 \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume $PWD:/workspace \
+    stankin_humble
